f832f975abc6e371c7bd6cb5846bca80a19bfcd8
Porson <porsonlee@gmail.com>
2020-03-12 02:10:00 -0700

    implemented search functionality with axios request to movie database api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from  'axios';
 
 import Search from "./components/Search"
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  })
+
+ const apiUrl = "https://www.omdbapi.com/?i=tt3896198&apikey=648e557b";
+
+ const search = (e) => {
+   if (e.key === "Enter") {
+    axios(apiUrl + "&s=" + state.s)
+      .then(( { data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+   }
+ }
+
+ const handleInput = (e) => {
+   let s = e.target.value; 
+
+   setState(prevState => {
+     return {...prevState, s: s}
+   })
+
+   console.log(state.s);
+ }
+
   return (
     <div className="App">
       <header>
         <h1>Movie Database</h1> 
       </header>
       <main>
-        <Search /> 
+        <Search handleInput={handleInput} search={search} /> 
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function Search () {
+function Search ({ handleInput , search }) {
   return (
     <section className="searchbox-wrap">
       <input 
         type="text"
         placeholder="Search for a movie..." 
         className="searchbox" 
+        onChange={handleInput}
+        onKeyPress={search}
       />
       
     </section>
