5eb2669a11e306670362c47bad447488799d198e
Porson <porsonlee@gmail.com>
2020-03-12 01:50:46 -0700

    set up css style

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Search () {
+  return (
+    <section className="searchbox-wrap">
+      <input 
+        type="text"
+        placeholder="Search for a movie..." 
+        className="searchbox" 
+      />
+      
+    </section>
+  )
+}
+
+export default Search; 
